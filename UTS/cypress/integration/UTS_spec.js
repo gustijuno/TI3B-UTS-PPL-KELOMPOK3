cf67266baaec13d2a22e0c9d9ef708a5bdd3161c
zidanshabira <zidanshabira@gmail.com>
2021-10-30 22:39:51 +0700

    Zidan Shabira As Sidiq Checkout Test

--- UTS/cypress/integration/UTS_spec.js
+++ UTS/cypress/integration/UTS_spec.js
@@ -45,6 +45,68 @@ context('Cypress Demo-Login', () =>{
         //Zidan Shabira (26)
         //Checkout test
 
+        // skenario 1 test backpack
+                /*cy.visit('https://www.saucedemo.com/')
+                cy.get('[data-test=username]').type('standard_user')
+                cy.get('[data-test=password]').type('secret_sauce')
+                cy.get('[data-test=login-button]').click()
+                cy.get('[data-test=add-to-cart-sauce-labs-backpack]').click()
+                cy.get('.shopping_cart_link').click()
+                cy.get('[data-test=checkout]').click()
+                cy.get('[data-test=firstName]').type('Zidan')
+                cy.get('[data-test=lastName]').type('Shabira')
+                cy.get('[data-test=postalCode]').type('000')
+                cy.get('[data-test=continue]').click()
+                cy.get('[data-test=finish]').click()
+                cy.get('[data-test=back-to-products]').click()*/
+        
+                // skenario 2 test kaos onesie
+                /*cy.visit('https://www.saucedemo.com/')
+                cy.get('[data-test=username]').type('standard_user')
+                cy.get('[data-test=password]').type('secret_sauce')
+                cy.get('[data-test=login-button]').click()
+                cy.get('[data-test=add-to-cart-sauce-labs-onesie]').click()
+                cy.get('.shopping_cart_link').click()
+                cy.get('[data-test=checkout]').click()
+                cy.get('[data-test=firstName]').type('Zidan')
+                cy.get('[data-test=lastName]').type('Shabira')
+                cy.get('[data-test=postalCode]').type('000')
+                cy.get('[data-test=continue]').click()
+                cy.get('[data-test=finish]').click()
+                cy.get('[data-test=back-to-products]').click()*/
+        
+                // skenario 3 test menggunakan username locked_out_user
+        
+                /*cy.visit('https://www.saucedemo.com/')
+                cy.get('[data-test=username]').type('locked_out_user')
+                cy.get('[data-test=password]').type('secret_sauce')
+                cy.get('[data-test=login-button]').click()*/
+        
+                // skenario 4 menggunakan username performance_glitch_user
+        
+                /*cy.visit('https://www.saucedemo.com/')
+                cy.get('[data-test=username]').type('performance_glitch_user')
+                cy.get('[data-test=password]').type('secret_sauce')
+                cy.get('[data-test=login-button]').click()
+                cy.get('[data-test=add-to-cart-sauce-labs-onesie]').click()
+                cy.get('.shopping_cart_link').click()
+                cy.get('[data-test=checkout]').click()*/
+        
+                // skenario 5 menggunakan username problem_user
+                /*cy.visit('https://www.saucedemo.com/')
+                cy.get('[data-test=username]').type('problem_user')
+                cy.get('[data-test=password]').type('secret_sauce')
+                cy.get('[data-test=login-button]').click()
+                cy.get('[data-test=add-to-cart-sauce-labs-onesie]').click()
+                cy.get('.shopping_cart_link').click()
+                cy.get('[data-test=checkout]').click()
+                cy.get('[data-test=firstName]').type('Zidan')
+                cy.get('[data-test=lastName]').type('Shabira')
+                cy.get('[data-test=postalCode]').type('000')
+                cy.get('[data-test=continue]').click()
+                cy.get('[data-test=finish]').click()
+                cy.get('[data-test=back-to-products]').click()*/
+
         
         //Rizal Whisnu Wiratama (23)
         //Sortir produk test  
