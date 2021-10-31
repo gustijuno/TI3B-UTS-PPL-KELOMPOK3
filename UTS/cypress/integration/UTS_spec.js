0e3ce1c47f1edf7749645df6f0a5a83f3173aad2
nadaalyak <nadaalya2424@gmail.com>
2021-10-31 21:19:13 +0700

    Nada Alya K

--- UTS/cypress/integration/UTS_spec.js
+++ UTS/cypress/integration/UTS_spec.js
@@ -77,6 +77,36 @@ context('Cypress Demo-Login', () =>{
 
         //Nada Alya Kamila (19)
         //Logout test
+
+        // skenario 1
+                /*cy.visit('https://www.saucedemo.com/')
+                cy.get('[data-test=username]').type('standard_user')
+                cy.get('[data-test=password]').type('secret_sauce')
+                cy.get('[data-test=login-button]').click()
+                cy.get('#react-burger-menu-btn').click()
+                cy.get('#logout_sidebar_link').click()
+        // skenario 2
+                /*cy.visit('https://www.saucedemo.com/')
+                cy.get('[data-test=username]').type('locked_out_user')
+                cy.get('[data-test=password]').type('secret_sauce')
+                cy.get('[data-test=login-button]').click()
+                cy.get('#react-burger-menu-btn').click()
+                cy.get('#logout_sidebar_link').click()
+        // skenario 3
+                /*cy.visit('https://www.saucedemo.com/')
+                cy.get('[data-test=username]').type('problem_user')
+                cy.get('[data-test=password]').type('secret_sauce')
+                cy.get('[data-test=login-button]').click()
+                cy.get('#react-burger-menu-btn').click()
+                cy.get('#logout_sidebar_link').click()
+        // skenario 4
+                /*cy.visit('https://www.saucedemo.com/')
+                cy.get('[data-test=username]').type('performance_glitch_user')
+                cy.get('[data-test=password]').type('secret_sauce')
+                cy.get('[data-test=login-button]').click()
+                cy.get('#react-burger-menu-btn').click()
+                cy.get('#logout_sidebar_link').click()
+
       
 
         //Zidan Shabira (26)
